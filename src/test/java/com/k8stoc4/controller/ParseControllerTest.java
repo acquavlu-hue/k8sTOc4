4929b02d7ce85a847fc59280e1d34854bfc35b51
Alex Speranza <asperanza@imolainformatica.it>
2026-03-09 10:41:53 +0100

    test: add test for ParseController

---
+++ src/test/java/com/k8stoc4/controller/ParseControllerTest.java
@@ -0,0 +1,33 @@
+package com.k8stoc4.controller;
+
+import com.k8stoc4.render.C4DslRenderer;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParseControllerTest {
+    private final String input;
+
+    public ParseControllerTest() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        this.input = classloader.getResource("controller/inputs/basic-input.yaml").getFile();
+    }
+
+    @Test
+    public void testBasicInput() {
+        final ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        final String expectedSpec = new BufferedReader(new InputStreamReader(Objects.requireNonNull(classloader.getResourceAsStream("controller/outputs/basic/spec.c4")))).lines().collect(Collectors.joining("\n"));
+        final String expectedModel = new BufferedReader(new InputStreamReader(Objects.requireNonNull(classloader.getResourceAsStream("controller/outputs/basic/model.c4")))).lines().collect(Collectors.joining("\n"));
+        final ParseController pc = new ParseController(this.input, Optional.empty(), Optional.empty());
+        final C4DslRenderer.Output renderOutput = pc.execute();
+
+        assertEquals(renderOutput.getSpec(), expectedSpec);
+        assertEquals(renderOutput.getModel(), expectedModel);
+    }
+}
